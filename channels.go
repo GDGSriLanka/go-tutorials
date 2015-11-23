c14a4aaedd10a59c5e584e0553c9eb1a53718c53
Raveen <harith9127@gmail.com>
2015-11-23 10:15:38 +0530

    channels added

--- channels.go
+++ channels.go
@@ -3,31 +3,46 @@ package main
 import (
   "fmt"
   "time"
-  "sync"
   "sync/atomic"
 )
 
-var wg sync.WaitGroup
+var counter int
+var myChannel = make(chan int)
+var flagChannel = make(chan bool)
+var x int64
 
 func main() {
-  wg.Add(2)
-  go count1()
-  go count2()
-  wg.Wait()
+  go addNum("First")
+  go addNum("Second")
+  go printChannelValue()
+  <-flagChannel
 }
 
-func count1()  {
-  for i := 0;i < 10;i++ {
-    fmt.Println("Count1 :" , i)
+func addNum(name string)  {
+  for i := 0;i < 10; i++ {
     time.Sleep(time.Millisecond*500)
+    fmt.Println(name , i)
+    myChannel <- 1
+    if i==9{
+      atomic.AddInt64(&x, 1)
+      fmt.Println(name, "is over and the flag value is",x)
+    }
+    if atomic.LoadInt64(&x) == 2{
+      close(myChannel)
+    }
   }
-  wg.Done()
 }
 
-func count2()  {
-  for i := 0;i < 10;i++ {
-    fmt.Println("Count2 :" , i)
-    time.Sleep(time.Millisecond*600)
+func printChannelValue()  {
+  for{
+    i, more := <- myChannel
+    if more{
+      counter += i
+      fmt.Println("Counter value is",counter)
+    } else {
+      flagChannel <- true
+      close(flagChannel)
+      return
+    }
   }
-  wg.Done()
 }
